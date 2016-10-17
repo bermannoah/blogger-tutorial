5bace1a6c76fa2245b430073555042400ba376bb
Noah Berman <noah@noahberman.org>
2016-10-16 18:03:36 -0600

    adds basic authentication

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,11 @@ Rails.application.routes.draw do
 	  resources :comments
   end
   resources :tags
+  resources :authors
+  resources :author_sessions, only: [ :new, :create, :destroy ]
+  
+  get 'login' => 'author_sessions#new'
+  get 'logout' => 'author_sessions#destroy'
   # Example resource route with options:
   #   resources :products do
   #     member do
